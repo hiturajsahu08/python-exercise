1f92f94395c335cdd4c618fc021579ff51ce4559
hiturajsahu08 <hiturajsahu08@gmail.com>
2020-10-03 15:15:58 +0530

    some more tkinter stuff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 import tkinter as tk
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -14,12 +15,13 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+p_value=8
 # tkinter exercise
 # init tk function
 parent = tk.Tk()
 # name a title for your window
 parent.title("Hello Window")
+
 # add label
 my_label = tk.Label(parent, text="My_Label", font=("Arial", 30))
 # position of label
@@ -31,12 +33,33 @@ my_label.grid(column=0,row=1)
 # add label
 my_label = tk.Label(parent, text="bottom_Label", font=("Arial", 15))
 # position of label
-my_label.grid(column=1,row=1)
+my_label.grid(column=0,row=2)
 # add label
 my_label = tk.Label(parent, text="Up_Label", font=("Arial", 35))
 # position of label
-my_label.grid(column=1,row=0)
+my_label.grid(column=0,row=3)
 
-parent.resizable(0,0)
+
+
+p = tk.Entry()
+p.grid(column=1,row=0)
+
+def save():
+    p_value=p.get()
+    print(p_value)
+#q=tk.messagebox.showinfo(title=None, message="Hello bro")
+
+# Open window having dimension 100x100
+# parent.geometry('100x100')
+# Create a Button
+#btn = tk.Button(parent, text='Click me !', bd='5',command=parent.destroy)
+btn = tk.Button(parent, text='Click me !', bd='5', command=save)
+
+# Set the position of button on the top of window.
+# btn.pack(side='bottom')
+# parent.resizable(0,0)
+# position of button
+btn.grid(column=2,row=0)
+
 # call it in loop
 parent.mainloop()
